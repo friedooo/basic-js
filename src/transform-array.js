3e03fda9aa2d21cc5e14dcaa33f6a4fd2526e2a7
friedooo <friedlich3@gmail.com>
2020-02-26 22:30:42 +0300

    feat: try to defeat transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,69 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (Array.isArray(arr) == false)
+    throw new Error();
+    let result = [];
+    let flagN = false;
+    let flagP = false;
+    let flagDN = false;
+    let flagDP = false;
+    for (let i = 0; i < arr.length; i++)
+    {
+        if (flagP = true)
+        {
+            result.unshift();
+        }
+
+        flagP = false;
+
+
+if (( arr[i] != '--discard-next' ||  arr[i] != '--discard-prev'  || arr[i] != '--double-next' ||  arr[i] != '--double-prev') && flagN == false)
+        {
+            if (flagDP == true)
+            {
+                result.push(arr[i-1]);
+                flagDP = false;
+            }
+            result.push(arr[i]);
+            if (flagDN == true)
+            {
+                result.push(arr[i]);
+                flagDN = false;
+            }
+        }
+///--discard-next
+        flagN = false;
+
+        if (arr[i] == '--discard-next')
+        {
+            flagN = true;
+        }
+///--discard-prev
+        if (arr[i] == '--discard-prev')
+        {
+            flagP = true;
+        }
+///--double-next
+
+        if (arr[i] == '--double-next')
+        {
+            flagDN = true;
+        }
+///--double-prev
+
+        if (arr[i] == '--double-prev')
+        {
+            flagDP = true;
+        }
+    }
+
+    for (let elem of result)
+    {
+        if (elem == '--discard-next' ||  elem == '--discard-prev'  || elem == '--double-next' ||  elem == '--double-prev')
+        {
+            console.log(arr);
+            break;
+        }
+    }
+    
+    return result;
 };
