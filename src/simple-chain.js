226cb24a88c24fb81243123bf24fe83439b5c7d9
friedooo <friedlich3@gmail.com>
2020-03-01 23:03:39 +0300

    feat: complete simple chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,31 +1,35 @@
 const chainMaker = {
-  'str': [],
-  getLength() {
-    return this.str.length;
-  },
-  addLink(elem) {
-      if (arguments[0] == undefined)
-      this.str.push('( )');
-      else
-      this.str.push(`( ${elem} )`);
-      return this;
-  },
-  removeLink(position) {
-      if (typeof position == 'number' && position%1 == 0)
-      {this.str.splice(position-1,1);
+    'str': [],
+    getLength() {
+      return this.str.length;
+    },
+    addLink(elem) {
+        if (arguments[0] == undefined)
+        this.str.push(`( ${null} )`);
+        else
+        this.str.push(`( ${elem} )`);
+        return this;
+    },
+    removeLink(position) {
+      if (typeof position == 'number' && position%1 == 0 && position > 0 && position <= this.getLength())
+      {
+          this.str.splice(position-1,1);
       return this;}
       else
+      {
+        this.str = [];
       throw new Error();
+      }
   },
-  reverseChain() {
-      this.str.reverse();
-      return this;
-  },
-  finishChain() {
-      let string = this.str.join('~~');
-      this.str = [];
-      return string;
-  }
-};
+    reverseChain() {
+        this.str.reverse();
+        return this;
+    },
+    finishChain() {
+        let string = this.str.join('~~');
+        this.str = [];
+        return string;
+    }
+  };
 
 module.exports = chainMaker;
