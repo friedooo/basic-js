078699f664ded00b28bfddb9beeacca617f563ea
friedooo <friedlich3@gmail.com>
2020-03-02 22:02:00 +0300

    feat: complete extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,49 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
-  
+module.exports = function repeater( str, options) {
+    let result = [];
+    let addition = [];
+    
+    if (str == null)
+    str = 'null';
+    
+    if (options.addition === null)
+    options.addition = 'null';
+    
+    if (typeof str != 'string')
+    str = str.toString();
+    
+    if (options.additionRepeatTimes == undefined)
+    options.additionRepeatTimes = 1;
+    
+    if (Number.isInteger(options.additionRepeatTimes))
+    {
+    for (let i = 0; i < options.additionRepeatTimes; i++)
+    {
+        addition.push(options.addition);
+    }
+    }
+    else
+    addition = [];
+    
+    if (options.additionSeparator == undefined)
+    options.additionSeparator = '!';
+    
+    let add = addition.join(options.additionSeparator);
+    
+    if (options.repeatTimes == undefined)
+    options.repeatTimes = 1;
+    
+    
+    if (Number.isInteger(options.repeatTimes))
+    {
+    for (let i = 0; i < options.repeatTimes; i++)
+    {
+        result.push(str + add);
+    }
+    }
+    
+    if (options.separator == undefined)
+    options.separator = '+';
+    
+    return result.join(options.separator);
+    };
+    
