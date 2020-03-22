5035e5bd1be371b2018abfc1d1fcfe8a215c27ae
friedooo <friedlich3@gmail.com>
2020-03-22 20:37:02 +0300

    feat: fix task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,70 +1,30 @@
 module.exports = function transform(arr) {
-    throw 'Not implemented';
-    if (Array.isArray(arr) == false)
-    throw new Error();
-    let result = [];
-    let flagN = false;
-    let flagP = false;
-    let flagDN = false;
-    let flagDP = false;
-    for (let i = 0; i < arr.length; i++)
-    {
-        if (flagP = true)
-        {
-            result.unshift();
-        }
-
-        flagP = false;
-
-
-if (( arr[i] != '--discard-next' ||  arr[i] != '--discard-prev'  || arr[i] != '--double-next' ||  arr[i] != '--double-prev') && flagN == false)
-        {
-            if (flagDP == true)
-            {
-                result.push(arr[i-1]);
-                flagDP = false;
-            }
-            result.push(arr[i]);
-            if (flagDN == true)
-            {
-                result.push(arr[i]);
-                flagDN = false;
-            }
-        }
-///--discard-next
-        flagN = false;
-
-        if (arr[i] == '--discard-next')
-        {
-            flagN = true;
-        }
-///--discard-prev
-        if (arr[i] == '--discard-prev')
-        {
-            flagP = true;
-        }
-///--double-next
-
-        if (arr[i] == '--double-next')
-        {
-            flagDN = true;
-        }
-///--double-prev
-
-        if (arr[i] == '--double-prev')
-        {
-            flagDP = true;
-        }
-    }
-
-    for (let elem of result)
-    {
-        if (elem == '--discard-next' ||  elem == '--discard-prev'  || elem == '--double-next' ||  elem == '--double-prev')
-        {
-            console.log(arr);
-            break;
-        }
-    }
+    if(!Array.isArray(arr)) throw 'arr is not an Array';
+    const transformKeys = ['--discard-next', '--discard-prev', '--double-next', '--double-prev']
     
-    return result;
+    const transformArray = [];
+    for (let index = 0; index < arr.length; index++) {
+        switch (arr[index]) {
+            case transformKeys[0]:
+                index += 1;
+                break;
+            case transformKeys[1]:
+                transformArray.pop();
+                break;
+            case transformKeys[2]:
+                if (arr.length > index + 1 && !transformKeys.includes(arr[index + 1])) {
+                    transformArray.push(arr[index + 1]);
+                }
+                break;
+            case transformKeys[3]:
+                if (index - 1 >= 0 && !transformKeys.includes(arr[index - 1])) {
+                    transformArray.push(arr[index - 1]);
+                }
+                break;
+            default:
+                transformArray.push(arr[index])
+                break;
+        }   
+    }
+    return transformArray;
 };
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,210 +1,95 @@
 class VigenereCipheringMachine {
-    constructor(direct)
-    {
-        this.direct = direct;
+
+    constructor(reverse = true) {
+        this.reverse = reverse;
+        this.tabulaRecta = {
+            "a": "abcdefghijklmnopqrstuvwxyz",
+            "b": "bcdefghijklmnopqrstuvwxyza",
+            "c": "cdefghijklmnopqrstuvwxyzab",
+            "d": "defghijklmnopqrstuvwxyzabc",
+            "e": "efghijklmnopqrstuvwxyzabcd",
+            "f": "fghijklmnopqrstuvwxyzabcde",
+            "g": "ghijklmnopqrstuvwxyzabcdef",
+            "h": "hijklmnopqrstuvwxyzabcdefg",
+            "i": "ijklmnopqrstuvwxyzabcdefgh",
+            "j": "jklmnopqrstuvwxyzabcdefghi",
+            "k": "klmnopqrstuvwxyzabcdefghij",
+            "l": "lmnopqrstuvwxyzabcdefghijk",
+            "m": "mnopqrstuvwxyzabcdefghijkl",
+            "n": "nopqrstuvwxyzabcdefghijklm",
+            "o": "opqrstuvwxyzabcdefghijklmn",
+            "p": "pqrstuvwxyzabcdefghijklmno",
+            "q": "qrstuvwxyzabcdefghijklmnop",
+            "r": "rstuvwxyzabcdefghijklmnopq",
+            "s": "stuvwxyzabcdefghijklmnopqr",
+            "t": "tuvwxyzabcdefghijklmnopqrs",
+            "u": "uvwxyzabcdefghijklmnopqrst",
+            "v": "vwxyzabcdefghijklmnopqrstu",
+            "w": "wxyzabcdefghijklmnopqrstuv",
+            "x": "xyzabcdefghijklmnopqrstuvw",
+            "y": "yzabcdefghijklmnopqrstuvwx",
+            "z": "zabcdefghijklmnopqrstuvwxy"
+        }
+    }
+    
+    encrypt(plainText, keyword) {
+        if( typeof(plainText) !== "string" ){
+            throw "invalid plainText. Must be string, not " + typeof(plainText);
+        }
+        if( typeof(keyword) !== "string" ){
+            throw "invalid keyword. Must be string, not " + typeof(keyword);
+        }
+    
+        plainText = plainText.toLowerCase();
+        keyword = keyword.match(/[a-z]/gi).join("").toLowerCase();
+        let encryptedText = "";
+        let specialCharacterCount = 0;
+    
+        for( let i = 0; i < plainText.length; i++ ){
+            const keyLetter = (i - specialCharacterCount) % keyword.length;
+            const keywordIndex = this.tabulaRecta.a.indexOf(keyword[keyLetter]);
+    
+            if( this.tabulaRecta[plainText[i]] ){
+                encryptedText += this.tabulaRecta[plainText[i]][keywordIndex];
+            }else{
+                encryptedText += plainText[i];
+                specialCharacterCount++;
+            }
+        }
+        
+        encryptedText = encryptedText.toUpperCase()
+
+        return this.reverse ? encryptedText : encryptedText.split('').reverse().join('');
     }
 
-    encrypt(string, key) {
-        if (string == undefined || key == undefined)
-        throw new Error();
-        let answer = '';
-
-        string = string.toUpperCase();
-        
-        let minusKey = -1;
-        let objPosofSigns = {};
-        for (let i = 0; i < string.length; i++)
-        {
-            if (string[i].charCodeAt(0) < 65 || string[i].charCodeAt(0) > 90)
-            {
-               // console.log(string[i] + ' ' + minusKey);
-               if (objPosofSigns[i + minusKey] != undefined)
-                {
-                    //console.log(string[i] + ' ' + (i + minusKey));
-                    objPosofSigns[i + minusKey] = objPosofSigns[i + minusKey] + string[i];
-                    //console.log(objPosofSigns[i + minusKey]);
-                }
-                else
-                objPosofSigns[i + minusKey] = string[i];
-                minusKey--;
+    decrypt(encryptedText, keyword) {
+        if( typeof(encryptedText) !== "string" ){
+            throw "invalid encryptedText. Must be string, not " + typeof(encryptedText);
+        }
+        if( typeof(keyword) !== "string" ){
+            throw "invalid keyword. Must be string, not " + typeof(keyword);
+        }
+      
+        encryptedText = encryptedText.toLowerCase();
+        keyword = keyword.match(/[a-z]/gi).join("").toLowerCase();
+        let decryptedText = "";
+        let specialCharacterCount = 0;
+      
+        for( let i = 0; i < encryptedText.length; i++ ){
+            const keyLetter = (i - specialCharacterCount) % keyword.length;
+            const keyRow = this.tabulaRecta[keyword[keyLetter]];
+    
+            if( keyRow.indexOf(encryptedText[i]) !== -1 ){
+                decryptedText += this.tabulaRecta.a[keyRow.indexOf(encryptedText[i])];
+            }else{
+                decryptedText += encryptedText[i];
+                specialCharacterCount++;
             }
         }
-        console.log(objPosofSigns);
-        string = string.replace(/([^A-Z])/g, '');
-        console.log(string);
+      
+        decryptedText = decryptedText.toUpperCase()
 
-
-        key = key.toUpperCase();
-        let keyWord = '';
-        if (string.length == key.length)
-        keyWord = key;
-        else if (string.length > key.length)
-        {
-            let counter = 0;
-            for (let i = 0; i < string.length; i++, counter++)
-            {
-                if (key[counter] == undefined)
-                counter = 0;
-
-                keyWord += key[counter];
-            }
-            console.log('ключевое слово меньше, чем слово кодировки = ' + keyWord);
-        }
-        else
-        {
-            for (let i = 0; i < key.length; i++)
-            {
-                if (string[i] == undefined)
-                {
-                    keyWord = key.slice(0, string.length - i);
-                }
-            }
-            console.log('ключевое слово больше, чем слово кодировки = ' + keyWord);
-        }
-
-        for (let i = 0; i < string.length; i++)
-        {
-            if (string[i].charCodeAt(0) >= 65 && string[i].charCodeAt(0) <= 90)
-            {
-            if (string[i].charCodeAt(0) - 65 + keyWord[i].charCodeAt(0) - 65 > 25)
-                {
-                    answer += String.fromCharCode(string[i].charCodeAt(0) - 65 + keyWord[i].charCodeAt(0) - 65 + 65 - 26); 
-                }
-                    else
-                {
-                    //console.log(string[i] + ' ' + keyWord[i]);
-                answer += String.fromCharCode((string[i].charCodeAt(0) - 65 + keyWord[i].charCodeAt(0) - 65) + 65);
-                }
-            }
-            else 
-            answer += string[i];
-            }
-  
-            answer = answer.split('');
-
-            for (let i = 0; i < answer.length; i++)
-            {
-               if (objPosofSigns[i] != undefined)
-                {
-                    console.log(answer[i]);
-                    console.log(objPosofSigns[i]);
-
-                    answer[i] += objPosofSigns[i];
-                }   
-            }
-            answer = answer.join('');
-        
-            if (this.direct == undefined)
-            return answer
-            if (this.direct == false)
-            return answer.split('').reverse().join('');
-        
-    }
-
-    ///
-    ///
-    ///
-    ///
-    ///
-
-    decrypt(string, key) {
-        if (string == undefined || key == undefined)
-        throw new Error();
-        let answer = '';
-
-        string = string.toUpperCase();
-        
-        let minusKey = -1;
-        let objPosofSigns = {};
-        for (let i = 0; i < string.length; i++)
-        {
-            if (string[i].charCodeAt(0) < 65 || string[i].charCodeAt(0) > 90)
-            {
-               // console.log(string[i] + ' ' + minusKey);
-               if (objPosofSigns[i + minusKey] != undefined)
-                {
-                    //console.log(string[i] + ' ' + (i + minusKey));
-                    objPosofSigns[i + minusKey] = objPosofSigns[i + minusKey] + string[i];
-                    //console.log(objPosofSigns[i + minusKey]);
-                }
-                else
-                objPosofSigns[i + minusKey] = string[i];
-                minusKey--;
-            }
-        }
-        console.log(objPosofSigns);
-        string = string.replace(/([^A-Z])/g, '');
-        console.log(string);
-
-
-        key = key.toUpperCase();
-        let keyWord = '';
-        if (string.length == key.length)
-        keyWord = key;
-        else if (string.length > key.length)
-        {
-            let counter = 0;
-            for (let i = 0; i < string.length; i++, counter++)
-            {
-                if (key[counter] == undefined)
-                counter = 0;
-
-                keyWord += key[counter];
-            }
-            console.log('ключевое слово меньше, чем слово кодировки = ' + keyWord);
-        }
-        else
-        {
-            for (let i = 0; i < key.length; i++)
-            {
-                if (string[i] == undefined)
-                {
-                    keyWord = key.slice(0, string.length - i);
-                }
-            }
-            console.log('ключевое слово больше, чем слово кодировки = ' + keyWord);
-        }
-
-        for (let i = 0; i < string.length; i++)
-        {
-            if (string[i].charCodeAt(0) >= 65 && string[i].charCodeAt(0) <= 90)
-            {
-            if (string[i].charCodeAt(0) - 65 >= keyWord[i].charCodeAt(0) - 65)
-                {
-                   // console.log(string[i] + ' ' + keyWord[i] + ' ' + 'выше диагонали');
-                    answer += String.fromCharCode(Math.abs(keyWord[i].charCodeAt(0) - 65 - (string[i].charCodeAt(0) - 65)) + 65); 
-                }
-                    else
-                {
-                    //console.log(string[i] + ' ' + keyWord[i] + ' ' + 'ниже диагонали');
-                //answer += String.fromCharCode((string[i].charCodeAt(0) - 65 - (keyWord[i].charCodeAt(0) - 65)) + 65);
-                //answer += String.fromCharCode(string[i].charCodeAt(0) - 65 + keyWord[i].charCodeAt(0) - 65 + 65 - 26)
-                answer += String.fromCharCode(65 + 26 - Math.abs(string[i].charCodeAt(0) - 65 - (keyWord[i].charCodeAt(0) - 65)));
-                }
-            }
-            else 
-            answer += string[i];
-            }
-  
-            answer = answer.split('');
-
-            for (let i = 0; i < answer.length; i++)
-            {
-               if (objPosofSigns[i] != undefined)
-                {
-                    console.log(answer[i]);
-                    console.log(objPosofSigns[i]);
-
-                    answer[i] += objPosofSigns[i];
-                }   
-            }
-            answer = answer.join('');
-        
-        if (this.direct == undefined)
-        return answer
-        if (this.direct == false)
-        return answer.split('').reverse().join('');
-        
+        return this.reverse ? decryptedText : decryptedText.split('').reverse().join('');
     }
 }
 
