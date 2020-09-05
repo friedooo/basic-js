3a06f2b6b1684a60489cd33d37f445de2bcd3cb8
friedooo <friedlich3@gmail.com>
2020-09-05 19:18:55 +0300

    feat: trying to indicate wrond estimated task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -26,4 +26,3 @@ module.exports = function dateSample(sActivity) {
   return false;
   return result;
 };
-
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,28 +1,28 @@
-module.exports = function transform(arr) {
-    if(!Array.isArray(arr)) throw 'arr is not an Array';
-    const Keys = ['--discard-next', '--discard-prev', '--double-next', '--double-prev']
+// module.exports = function transform(arr) {
+//     if(!Array.isArray(arr)) throw 'arr is not an Array';
+//     const Keys = ['--discard-next', '--discard-prev', '--double-next', '--double-prev']
     
-    const transformArray = [];
-    for (let index = 0; index < arr.length; index++) {
+//     const transformArray = [];
+//     for (let index = 0; index < arr.length; index++) {
 
-        if (arr[index] == Keys[0])
-        index += 1;
-        else if (arr[index] == Keys[1])
-        transformArray.pop();
-        else if (arr[index] == Keys[2])
-        {
-            if (arr.length > index + 1 && !Keys.includes(arr[index + 1])) {
-                transformArray.push(arr[index + 1]);
-            }
-        }
-        else if (arr[index] == Keys[3])  
-        {
-            if (index - 1 >= 0 && !Keys.includes(arr[index - 1])) {
-                transformArray.push(arr[index - 1]);
-            }
-        }
-        else
-        transformArray.push(arr[index])         
-    }
-    return transformArray;
-};
+//         if (arr[index] == Keys[0])
+//         index += 1;
+//         else if (arr[index] == Keys[1])
+//         transformArray.pop();
+//         else if (arr[index] == Keys[2])
+//         {
+//             if (arr.length > index + 1 && !Keys.includes(arr[index + 1])) {
+//                 transformArray.push(arr[index + 1]);
+//             }
+//         }
+//         else if (arr[index] == Keys[3])  
+//         {
+//             if (index - 1 >= 0 && !Keys.includes(arr[index - 1])) {
+//                 transformArray.push(arr[index - 1]);
+//             }
+//         }
+//         else
+//         transformArray.push(arr[index])         
+//     }
+//     return transformArray;
+// };
